65de43b7b8b43e439fda26468c561d8be34bfd99
unknown <remek778790@gmail.com>
2022-05-08 10:57:46 +0200

    initial deploy

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import SideBar from "./components/sidebar/SideBar";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App_glass">
+        <SideBar/>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/sidebar/SideBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./SideBar.css";
+
+const SideBar = () => {
+  return (
+    <div className="sideBar">
+      <div className="sideBar_logo">
+        <img src="#" alt="sidebar_logo" />
+        <span>
+          Sh<span>o</span>ps
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
