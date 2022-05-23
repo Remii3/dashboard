5c50c27ff5bcffdf816ea9f67b6711868d7ace65
unknown <remek778790@gmail.com>
2022-05-23 12:14:26 +0200

    sideBar style changes, new Icons

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { SidebarData } from "../../data/data";
 import "./SideBar.css";
-
+import { UilClipboard, UilSignOutAlt } from "@iconscout/react-unicons";
 const SideBar = () => {
+  const [selected, setSelected] = useState(0);
+
+  const selectMenuTabHandler = (indexValue) => {
+    setSelected(indexValue);
+  };
+
   return (
     <div className="sideBar">
       <div className="sideBar_logo">
-        <img src="#" alt="sidebar_logo" />
+        <UilClipboard />
         <span>
           Sh<span>o</span>ps
         </span>
       </div>
       <div className="sideBar_menu">
+        {SidebarData.map((item, index) => (
+          <div
+            className={`menu_item ${selected === index && "active"}`}
+            key={index}
+            onClick={() => selectMenuTabHandler(index)}
+          >
+            <item.icon />
+            <span>{item.heading}</span>
+          </div>
+        ))}
         <div className="menu_item">
-          <div>Icon</div>
-          <span>Dashboard</span>
+          <UilSignOutAlt />
         </div>
       </div>
     </div>
---
+++ src/data/data.js
@@ -0,0 +1,15 @@
+import {
+  UilEstate,
+  UilClipboard,
+  UilUsersAlt,
+  UilPackage,
+  UilChart,
+} from "@iconscout/react-unicons";
+
+export const SidebarData = [
+  { icon: UilEstate, heading: "Dashboard" },
+  { icon: UilClipboard, heading: "Orders" },
+  { icon: UilUsersAlt, heading: "Customers" },
+  { icon: UilPackage, heading: "Products" },
+  { icon: UilChart, heading: "Analytics" },
+];
